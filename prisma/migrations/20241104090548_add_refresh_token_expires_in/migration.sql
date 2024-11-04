ffd7bd4b2841b9c6d67a6ffc80636c0470596427
pcaboor <pierre.caboor59@gmail.com>
2024-11-04 11:55:37 +0100

    add prisma/

---
+++ prisma/migrations/20241104090548_add_refresh_token_expires_in/migration.sql
@@ -0,0 +1,20 @@
+-- DropIndex
+DROP INDEX `accounts_userId_fkey` ON `accounts`;
+
+-- DropIndex
+DROP INDEX `posts_authorId_fkey` ON `posts`;
+
+-- DropIndex
+DROP INDEX `sessions_userId_fkey` ON `sessions`;
+
+-- AlterTable
+ALTER TABLE `accounts` ADD COLUMN `refresh_token_expires_in` INTEGER NULL;
+
+-- AddForeignKey
+ALTER TABLE `accounts` ADD CONSTRAINT `accounts_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `sessions` ADD CONSTRAINT `sessions_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `posts` ADD CONSTRAINT `posts_authorId_fkey` FOREIGN KEY (`authorId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
