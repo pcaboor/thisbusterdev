6f8e61d51164fe66d6ca00a2b13c8a33ee7accc3
pcaboor <pierre.caboor59@gmail.com>
2024-11-04 11:51:07 +0100

    add app/

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,51 @@
+import Link from "next/link"
+
+import { marketingConfig } from "@/config/marketing"
+import { cn } from "@/lib/utils"
+import { buttonVariants } from "@/components/ui/button"
+import { MainNav } from "@/components/main-nav"
+import { SiteFooter } from "@/components/site-footer"
+import { getCurrentUser } from "@/lib/session"
+import { UserAvatar } from "@/components/user-avatar"
+import { User } from "next-auth"
+
+interface MarketingLayoutProps {
+  children: React.ReactNode
+}
+
+export default async function MarketingLayout({
+  children,
+}: MarketingLayoutProps) {
+  const user = await getCurrentUser()
+
+  return (
+    <div className="flex min-h-screen flex-col">
+      <header className="container z-40 bg-background">
+        <div className="flex h-20 items-center justify-between py-6">
+          <MainNav items={marketingConfig.mainNav} />
+          <nav>
+            {user ? (
+              <UserAvatar
+                href="/dashboard"
+                user={{ name: user.name || null, image: user.image || null }}
+                className="h-8 w-8"
+              />
+            ) : (
+              <Link
+                href="/login"
+                className={cn(
+                  buttonVariants({ variant: "secondary", size: "sm" }),
+                  "px-4"
+                )}
+              >
+                Login
+              </Link>
+            )}
+          </nav>
+        </div>
+      </header>
+      <main className="flex-1">{children}</main>
+      <SiteFooter />
+    </div>
+  )
+}
